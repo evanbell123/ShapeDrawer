b15b49b30fd12cca79c4d0c39cfdbbfd04795528
Evan Bell <evanbell91@gmail.com>
2016-01-29 10:49:53 -0600

    drawing shapes works

--- src/DrawingApplication/DrawingApplication.java
+++ src/DrawingApplication/DrawingApplication.java
@@ -5,17 +5,69 @@
  */
 package DrawingApplication;
 
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import javax.swing.JFrame;
+
 /**
  *
  * @author ebbmf
  */
-public class DrawingApplication {
+public class DrawingApplication extends JFrame {
+    
+    private final LinkedList<Shape> shapes;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        DrawingApplication shapeDrawer = new DrawingApplication();
+        shapeDrawer.setVisible(true);
     }
+
+    public DrawingApplication() {
+        
+        prepareGUI();
+        
+        this.shapes = new LinkedList<>();
+        
+        shapes.add(new Ellipse(100, 100, 50, 50));
+        shapes.add(new Ellipse(100, 75, 50, 50));
+        shapes.add(new Ellipse(100, 125, 50, 50));
+        
+        shapes.add(new Ellipse(75, 100, 50, 50));
+        shapes.add(new Ellipse(100, 100, 50, 50));
+        shapes.add(new Ellipse(125, 100, 50, 50));
+        
+        shapes.add(new Ellipse(82, 82, 50, 50));
+        shapes.add(new Ellipse(118, 82, 50, 50));
+        shapes.add(new Ellipse(82, 118, 50, 50));
+        shapes.add(new Ellipse(118, 118, 50, 50));
+        
+        shapes.add(new Ellipse(75, 75, 100, 100));
+        shapes.add(new Rectangle(75, 75, 100, 100));
+    }
+    
+    @Override
+    public void paint(Graphics g) {
+        ListIterator<Shape> shapeIter = shapes.listIterator();
+        
+        while (shapeIter.hasNext()) {
+            shapeIter.next().draw(g);
+        }
+    }
+    
+    private void prepareGUI(){
+      setSize(400,400);
+      addWindowListener(new WindowAdapter() {
+         @Override
+         public void windowClosing(WindowEvent windowEvent){
+            System.exit(0);
+         }        
+      }); 
+   }  
     
 }
